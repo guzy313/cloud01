c119767772ec816a5759b0da5a24cf84501b9877
顾哲宇 <guzy@yeyoo.cn>
2022-12-21 20:12:26 +0800

    支付模块初始环境搭建完成

---
+++ cloud-provider-payment8001/src/main/java/com/my/springcloud/PayMentMain8001.java
@@ -0,0 +1,19 @@
+package com.my.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gzy
+ * @version 1.0
+ * @Description
+ */
+@SpringBootApplication
+public class PayMentMain8001 {
+
+    public static void  main(String[] args){
+        SpringApplication.run(PayMentMain8001.class,args);
+    }
+
+}
