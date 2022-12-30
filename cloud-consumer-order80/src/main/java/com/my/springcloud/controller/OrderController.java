4d48f6dcc3f9d6de8b5e24f37efae5fe9ae72c91
顾哲宇 <guzy@yeyoo.cn>
2022-12-30 16:38:29 +0800

    消费者模块远程调用支付模块成功

--- cloud-consumer-order80/src/main/java/com/my/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/my/springcloud/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.my.springcloud.controller;
 import com.my.springcloud.entity.CommonResult;
 import com.my.springcloud.entity.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import sun.dc.pr.PRError;
@@ -10,7 +12,7 @@ import sun.dc.pr.PRError;
 /**
  * @author Gzy
  * @version 1.0
- * @Description
+ * @Description: 消费者模块控制器
  */
 @RestController
 public class OrderController {
@@ -20,10 +22,15 @@ public class OrderController {
     private RestTemplate restTemplate;
 
 
+    @GetMapping("/consumer/payment/add")
     public CommonResult<Payment> add(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL + "/payment/add",payment,CommonResult.class);
     }
 
 
+    @GetMapping("/consumer/payment/findById/{id}")
+    public CommonResult<Payment> findById(@PathVariable("id")long id){
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/findById/"+id,CommonResult.class);
+    }
 
 }
