15ce92d8698a3dcc67270585e9f1131d3af20525
guzy <313791949@qq.com>
2023-01-29 12:10:03 +0800

    springcloud gateway Demo module: cloud-gateway-gateway9527 完成

---
+++ cloud-gateway-gateway9527/src/main/java/com/my/springcloud/GatewayMain9527.java
@@ -0,0 +1,21 @@
+package com.my.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Gzy
+ * @version 1.0
+ * @Description
+ * @date create on 2023/1/29
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+
+}
