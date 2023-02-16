79c3f9bf2d662a6e52542a1c819d51f27032b559
guzy <313791949@qq.com>
2023-02-16 20:28:00 +0800

    cloud-config-client-3355 配置中心客户端demo模块3355创建 并且成功调用 3344 server 配置中心服务端 获取到配置文件内容 并且在 ConfigClientController 中以REST的形式暴露 测试用URL在浏览器访问成功

---
+++ cloud-config-client-3355/src/main/java/com/my/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.my.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Gzy
+ * @version 1.0
+ * @Description 将配置信息以REST接口的形式暴露
+ * @date create on 2023/2/16
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
