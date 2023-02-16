509b70f7bf2406eda9884f8cf98db4dc06250fcb
guzy <313791949@qq.com>
2023-02-16 11:40:58 +0800

    cloud-config-center-3344 SpringCloud Config服务端模块创建 成功启动

---
+++ cloud-config-center-3344/src/main/java/com/my/springcloud/SpringCloudConfigMain.java
@@ -0,0 +1,23 @@
+package com.my.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Gzy
+ * @version 1.0
+ * @Description
+ * @date create on 2023/2/15
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class SpringCloudConfigMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigMain.class,args);
+    }
+
+}
