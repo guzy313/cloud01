f58da88f732d4e546a1e7eca83e49070324d71db
guzy <313791949@qq.com>
2023-02-17 11:51:14 +0800

    cloud-config-client-3355 配置中心客户端demo模块-实现动态获取配置中心服务端github上的配置文件

--- cloud-config-client-3355/src/main/java/com/my/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/my/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.my.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date create on 2023/2/16
  */
 @RestController
+@RefreshScope //动态刷新
 public class ConfigClientController {
 
     @Value("${config.info}")
