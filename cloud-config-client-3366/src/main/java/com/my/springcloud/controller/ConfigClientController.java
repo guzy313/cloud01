83e1a41da8408ed8f0e656de9766bef4dc599678
guzy <313791949@qq.com>
2023-02-18 12:44:45 +0800

    cloud-config-client-3366 配置中心客户端demo模块新建 与3355相似，用来测试Bus广播

---
+++ cloud-config-client-3366/src/main/java/com/my/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.my.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Gzy
+ * @version 1.0
+ * @Description
+ * @date create on 2023/2/18
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
